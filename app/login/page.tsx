b205185fc8b75c2f37bc3d7482260b2b69db3091
Arun <arun.prabhakaran@skystack.io>
2024-02-29 19:29:09 +0530

    Added new login page ui in login component.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -9,14 +9,14 @@ import { LoaderIcon, MessageIcon } from "@/Utility/icons/icons"
 import { SocialappStore } from "@/Store/SocialappStore"
 import Notification from "@/Utility/Uicomponents/Notifications/Notification"
 import useGetFonts from "@/font/fonts"
-import loginImage from "../../Components/images/loginImage.gif"
+import loginBanner from "../../Components/images/geometric-shapes-background-flat-design_23-2148373329.jpg"
 import Image from "next/image"
 interface FormTypes {
   email: string
   password: string
 }
 function LoginPage() {
-  const loginBanner: any = loginImage.src
+  const loginImage: any = loginBanner.src
   const [isShowPass, setpass] = useState(false)
   const { ContentFont, HeaderFont } = useGetFonts()
   const setNotification = SocialappStore((state: any) => state.setNotification)
@@ -63,98 +63,106 @@ function LoginPage() {
   return (
     <>
       <Notification />
-      <div className="bg-[#09090b] w-full min-h-screen grid place-items-center p-3">
-        <div className="p-3 rounded-lg shadow-2xl max-w-[450px] w-full bg-[#fff]">
-          <div className="flex justify-center">
-            <button className="rounded-lg w-fit">
-              {/* <MessageIcon width={20} /> */}
-              <Image
-                src={loginBanner}
-                alt="loginbanner"
-                width={100}
-                height={100}
-                className="object-contain"
-              />
-            </button>
+      <div className="bg-white w-full min-h-screen grid place-items-center   gap-1 p-3">
+        <div className="grid lg:grid-cols-2 gap-2">
+          <div className="hidden lg:block bg-white max-w-[400px]">
+            <img
+              src={loginImage}
+              alt="banner image"
+              className="object-cover w-full h-full"
+            />
           </div>
-          <h1
-            className={`text-3xl text-center py-2 lg:py-5 font-bold text-[#42434d] ${HeaderFont.className}`}
-          >
-            Sign in to chat
-          </h1>
-          <p className={`text-center text-[#7c7e8d] ${ContentFont.className}`}>
-            Welcome back! Please sign in to continue
-          </p>
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className={`w-full h-full rounded  pt-3 lg:pt-10 ${ContentFont.className}`}
-          >
-            <label
-              htmlFor="email"
-              className={`py-3 block text-[#42434d] font-semibold`}
-            >
-              Email :
-              <small className="capitalize tracking-wide text-red-500 px-1">
-                {errors?.email?.message}
-              </small>
-            </label>
-            <Input
-              register={register}
-              required={true}
-              pattern={/^[^\s@]+@[^\s@]+\.[^\s@]+$/}
-              name="email"
-              className={`border text-[#42434d] ${
-                errors?.email?.message ? "border-red-500" : "border-[#e6e6e7]"
-              } rounded-lg p-2 outline-none bg-inherit w-full`}
-              type="text"
-            />
+          <div className="grid place-items-center p-3 bg-white w-full">
+            <div className="min-w-[400px]">
+              <h1
+                className={`text-[3rem] text-center py-2 lg:py-5 font-bold text-[#42434d] ${HeaderFont.className}`}
+              >
+                Sign In
+              </h1>
+              <p
+                className={`text-center text-[#7c7e8d] ${ContentFont.className}`}
+              >
+                Welcome back! Please sign in to continue
+              </p>
+              <form
+                onSubmit={handleSubmit(onSubmit)}
+                className={`w-full h-full rounded  pt-3 lg:pt-10 ${ContentFont.className}`}
+              >
+                <label
+                  htmlFor="email"
+                  className={`py-3 block text-[#42434d] font-semibold`}
+                >
+                  Email :
+                  <small className="capitalize tracking-wide text-red-500 px-1">
+                    {errors?.email?.message}
+                  </small>
+                </label>
+                <Input
+                  register={register}
+                  required={true}
+                  pattern={/^[^\s@]+@[^\s@]+\.[^\s@]+$/}
+                  name="email"
+                  className={`border text-[#42434d] ${
+                    errors?.email?.message
+                      ? "border-red-500"
+                      : "border-[#e6e6e7]"
+                  } rounded-lg p-2 outline-none bg-inherit w-full`}
+                  type="text"
+                />
 
-            <label
-              htmlFor="password"
-              className={`py-3 block text-[#42434d] font-semibold`}
-            >
-              Password :{" "}
-              <small className="capitalize tracking-wide text-red-500">
-                {errors?.password?.message}
-              </small>
-            </label>
-            <Input
-              register={register}
-              required={true}
-              pattern={/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/}
-              name="password"
-              className={`border text-[#42434d] ${
-                errors?.password?.message
-                  ? "border-red-500"
-                  : "border-[#e6e6e7]"
-              } rounded-lg p-2 outline-none  bg-inherit w-full `}
-              type={isShowPass ? "text" : "password"}
-              handleShowPass={() => setpass(!isShowPass)}
-            />
+                <label
+                  htmlFor="password"
+                  className={`py-3 block text-[#42434d] font-semibold`}
+                >
+                  Password :{" "}
+                  <small className="capitalize tracking-wide text-red-500">
+                    {errors?.password?.message}
+                  </small>
+                </label>
+                <Input
+                  register={register}
+                  required={true}
+                  pattern={/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/}
+                  name="password"
+                  className={`border text-[#42434d] ${
+                    errors?.password?.message
+                      ? "border-red-500"
+                      : "border-[#e6e6e7]"
+                  } rounded-lg p-2 outline-none  bg-inherit w-full `}
+                  type={isShowPass ? "text" : "password"}
+                  handleShowPass={() => setpass(!isShowPass)}
+                />
 
-            <div className="pt-5 grid lg:grid-cols-2  gap-2">
-              <Button
-                type="button"
-                label="back"
-                onClick={() => router.back()}
-                className="bg-[#e9e9ea] rounded-lg py-2 order-1 lg:order-none"
-              />
-              <Button
-                type="submit"
-                label="Login"
-                className="bg-[#3c3c43] rounded-lg py-2 hover:shadow-2xl text-white transition-shadow duration-200"
-              />
-              <span className="lg:col-span-2 py-2 text-center text-xs">
-                Dont&lsquo; have account ?
-              </span>
-              <Button
-                onClick={() => router.push("/signUp")}
-                type="button"
-                label="Register"
-                className="bg-[#7F27FF] text-white rounded-lg py-2 hover:shadow-lg hover:shadow-indigo-600/40 transition-shadow duration-200 lg:col-span-2"
-              />
+                <div className="pt-5 grid lg:grid-cols-2  gap-2">
+                  <Button
+                    type="button"
+                    label="Back"
+                    onClick={() => router.back()}
+                    className="bg-[#191919] text-white rounded-lg py-2 order-1 lg:order-none"
+                  />
+                  <Button
+                    type="submit"
+                    label="Login"
+                    className="bg-[#00c28e] rounded-lg py-2 hover:shadow-lg hover:shadow-[#00c28e]/50 text-white transition-shadow duration-200"
+                  />
+                  <Button
+                    type="button"
+                    label="Continue with Google"
+                    className="bg-gradient-to-r from-[#00c28e] via-indigo-500 to-blue-700 bg-clip-text text-transparent border rounded-lg py-2 hover:shadow-lg transition-shadow duration-200 col-span-2"
+                  />
+                  <span className="lg:col-span-2 py-2 text-center text-xs">
+                    Dont&lsquo; have account ?{" "}
+                    <span
+                      onClick={() => router.push("/signUp")}
+                      className="text-[#1152ee] cursor-pointer"
+                    >
+                      Sign Up
+                    </span>
+                  </span>
+                </div>
+              </form>
             </div>
-          </form>
+          </div>
         </div>
       </div>
     </>
