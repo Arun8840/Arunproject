6a8d56eca5cb807b476d159d5ba8443928ac1654
Arun <arunprakashrani@gmail.com>
2024-04-28 22:24:43 +0530

    added new dark and light theme for landing page.

--- Components/HomeComponents/LandingPage.tsx
+++ Components/HomeComponents/LandingPage.tsx
@@ -42,7 +42,7 @@ function LandingPage() {
       {
         scale: 2,
         ease: Expo.easeOut,
-        duration: 0.5,
+        duration: 1,
         stagger: 0.2,
       }
     )
@@ -112,10 +112,10 @@ function LandingPage() {
         </div>
       </div>
       <div ref={gridScreen} className="w-full h-full bg-black hidden opacity-0">
-        <section className="bg-white dark:bg-[#101010] transition-colors duration-300  min-h-screen  grid lg:grid-cols-12 auto-rows-max gap-5  p-3">
+        <section className="bg-[#fff] dark:bg-[#101010] transition-colors duration-300  min-h-screen  grid lg:grid-cols-12 auto-rows-max gap-5  p-3">
           <>
             {/* //todo header */}
-            <div className="bg-[#f8fe9d] w-full h-full backdrop-blur-sm   p-3 lg:col-span-6 flex items-center justify-center flex-wrap gap-5  rounded-3xl">
+            <div className="bg-white border dark:border-none dark:bg-[#f8fe9d] w-full h-full backdrop-blur-sm   p-3 lg:col-span-6 flex items-center justify-center flex-wrap gap-5  rounded-3xl">
               <div className="hidden lg:block w-1/5 h-full">
                 <Image
                   width={200}
@@ -133,7 +133,7 @@ function LandingPage() {
                   Hi i&apos;m arun_
                 </h1>
                 <p
-                  className={`${ContentFont?.className} capitalize tracking-wider py-2 text-sm leading-7`}
+                  className={`${ContentFont?.className} capitalize tracking-wider py-2 text-sm leading-7 text-gray-600 dark:text-black`}
                 >
                   frondend developer, <br /> currently working at zettastack
                   systems PVT. I design and code beautifully simple things, and
@@ -143,7 +143,7 @@ function LandingPage() {
             </div>
 
             {/* //todo about */}
-            <div className="bg-[#4842fe]  p-3 lg:col-span-4 text-white   rounded-3xl">
+            <div className="bg-white border dark:border-none text-black  dark:text-white dark:bg-[#4842fe]  p-3 lg:col-span-4    rounded-3xl">
               <div className="divide-y divide-stone-700 divide-opacity-35">
                 {" "}
                 <h1
@@ -152,7 +152,7 @@ function LandingPage() {
                   what i&apos;m about?
                 </h1>
                 <p
-                  className={` ${ContentFont?.className} capitalize tracking-wider px-1 py-3 text-sm leading-7`}
+                  className={` ${ContentFont?.className} capitalize tracking-wider px-1 py-3 text-sm leading-7 text-gray-600 dark:text-white`}
                 >
                   {" "}
                   My journey in web development began ZettaStack, which has
@@ -163,7 +163,7 @@ function LandingPage() {
             </div>
 
             {/* //todo settings icons */}
-            <div className="bg-[#d1fe06] dark:bg-white text-[#d1fe06] w-fit mx-auto lg:w-full h-full  p-2 lg:col-span-2  grid place-items-center gap-1 rounded-3xl">
+            <div className="bg-white border dark:border-none dark:bg-[#d1fe06] text-[#d1fe06] w-fit mx-auto lg:w-full h-full  p-2 lg:col-span-2  grid place-items-center gap-1 rounded-3xl">
               {!darkMode ? (
                 <button
                   onClick={toggleDarkMode}
@@ -182,7 +182,7 @@ function LandingPage() {
             </div>
 
             {/* //todo projects */}
-            <div className="bg-[#DCFFB7] text-[black]  lg:col-span-5 rounded-3xl p-3">
+            <div className="bg-white  border dark:border-none dark:bg-[#DCFFB7] text-[black]  lg:col-span-5 rounded-3xl p-3">
               <h1
                 className={`text-[2rem] ${HeaderFont?.className} capitalize tracking-wider p-2 `}
               >
@@ -203,14 +203,15 @@ function LandingPage() {
                             </h1>
                           </div>
                           <p
-                            className={`${ContentFont?.className} capitalize tracking-wider line-clamp-2 text-sm px-2  leading-7`}
+                            className={`${ContentFont?.className} capitalize tracking-wider line-clamp-2 text-sm px-2  leading-7 text-gray-600 dark:text-black`}
                           >
                             {proItems?.description}
                           </p>
-                          <div className="flex justify-end">
+                          <div className="flex justify-end pt-3">
                             <Link href={proItems?.path}>
-                              <button className="text-sm bg-black  p-1 rounded-full text-white">
-                                <RightArrow width={18} />
+                              <button className="text-sm tracking-wider font-semibold flex items-center gap-2">
+                                Open
+                                <RightArrow width={15} />
                               </button>{" "}
                             </Link>
                           </div>
@@ -220,6 +221,8 @@ function LandingPage() {
                 </ul>
               </div>
             </div>
+
+            {/* experience */}
             <div className="bg-[white] grid place-items-center  lg:col-span-3  rounded-3xl overflow-hidden">
               <Image
                 width={400}
@@ -231,7 +234,7 @@ function LandingPage() {
             </div>
 
             <div className="lg:col-span-4  flex flex-col justify-between gap-3">
-              <div className="bg-[#f8fe9d]  p-3 col-span-4 grid lg:grid-cols-3 auto-rows-max gap-3 rounded-3xl">
+              <div className="bg-white  border dark:border-none dark:bg-[#f8fe9d]  p-3 col-span-4 grid lg:grid-cols-3 auto-rows-max gap-3 rounded-3xl">
                 <div className={`text-center  flex flex-col justify-between `}>
                   <h1 className="text-[3rem]  font-semibold font-mono">2+</h1>
                   <p
@@ -257,7 +260,7 @@ function LandingPage() {
                   </p>
                 </div>
               </div>
-              <div className="grid grid-cols-4 place-items-center bg-[white] rounded-3xl flex-1 p-3">
+              <div className="grid border dark:border-none grid-cols-4 place-items-center bg-[white] rounded-3xl flex-1 p-3">
                 {" "}
                 {SkillItems?.length > 0 &&
                   SkillItems?.map((items, index: number) => {
@@ -274,14 +277,14 @@ function LandingPage() {
             </div>
 
             {/* //todo sample content */}
-            <div className="bg-[#FF8400] p-3 lg:col-span-6 text-white   rounded-3xl">
+            <div className="bg-white border dark:border-none dark:bg-[#FF8400] p-3 lg:col-span-6 dark:text-white   rounded-3xl">
               <h1
                 className={`text-[1.5rem] ${HeaderFont?.className} capitalize tracking-wider p-1`}
               >
                 what i do best
               </h1>
               <p
-                className={` ${ContentFont?.className} capitalize tracking-wider px-1 py-2 text-sm leading-7`}
+                className={` ${ContentFont?.className} capitalize tracking-wider px-1 py-2 text-sm leading-7 text-gray-600 dark:text-white`}
               >
                 I am an enthusiastic Frontend Developer with a strong penchant
                 for creating elegant and responsive user interfaces.{" "}
@@ -289,14 +292,14 @@ function LandingPage() {
             </div>
 
             {/* contact */}
-            <div className="bg-[#D67BFF]  p-3 lg:col-span-6 flex flex-col justify-between rounded-3xl">
+            <div className="bg-white border dark:border-none  dark:bg-[#D67BFF]  p-3 lg:col-span-6 flex flex-col justify-between rounded-3xl">
               <h1
                 className={`text-[1.5rem] ${HeaderFont?.className} capitalize tracking-wider p-1`}
               >
                 Contact
               </h1>
               <p
-                className={` ${ContentFont?.className} capitalize tracking-wider px-1 py-2 text-sm leading-7`}
+                className={` ${ContentFont?.className} capitalize tracking-wider px-1 py-2 text-sm leading-7 text-gray-600 dark:text-black`}
               >
                 Ready to enhance your website&apos;s user experience? Contact me
                 today to discuss your front-end development needs.
@@ -307,10 +310,10 @@ function LandingPage() {
               >
                 <input
                   type="text"
-                  className="bg-white outline-none rounded-2xl bg-transparent p-3 flex-1 w-full lg:w-fit placeholder:text-black"
+                  className="bg-white outline-none border dark:border-none rounded-2xl bg-transparent p-3 flex-1 w-full lg:w-fit placeholder:text-black"
                   placeholder="Your Email address"
                 />
-                <button className="bg-[white] rounded-2xl  transition-colors duration-200 p-3 w-full lg:w-fit">
+                <button className="bg-[#101010] dark:bg-[#10a958] text-white rounded-2xl  transition-colors duration-200 p-3 w-full lg:w-fit">
                   Get in touch !
                 </button>
               </div>
