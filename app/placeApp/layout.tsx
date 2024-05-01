dc78f6d382b2773e08797b25279876ec0eb95818
Arun <arunprakashrani@gmail.com>
2024-05-01 16:33:16 +0530

    useref variable issue clear when deploy.

--- app/placeApp/layout.tsx
+++ app/placeApp/layout.tsx
@@ -11,28 +11,28 @@ interface MenuTypes {
 }
 
 function layout({ children }: { children: React.ReactNode }) {
-  let splashScreen = useRef(null)
-  let mainScreen = useRef(null)
-  let splashIcon = useRef(null)
+  let SplashScreen = useRef(null)
+  let MainScreen = useRef(null)
+  let SplashIcon = useRef(null)
 
   useEffect(() => {
     const timeLine = gsap.timeline({ paused: false })
-    timeLine.to(splashIcon?.current, {
+    timeLine.to(SplashIcon?.current, {
       scale: 1,
       duration: 1,
       ease: Expo.easeOut,
     })
-    timeLine.to(splashIcon?.current, {
+    timeLine.to(SplashIcon?.current, {
       scale: 0,
       duration: 0.8,
       ease: Expo.easeIn,
       onComplete: () => {
-        gsap.to(splashScreen.current, {
+        gsap.to(SplashScreen.current, {
           opacity: 0,
           display: "none",
           duration: 0.5,
           onComplete: () => {
-            gsap.to(mainScreen.current, {
+            gsap.to(MainScreen.current, {
               opacity: 1,
               display: "flex",
               duration: 0.5,
@@ -61,18 +61,18 @@ function layout({ children }: { children: React.ReactNode }) {
     <div className="w-full min-h-screen max-h-screen bg-white dark:bg-[#1a1a1a] grid">
       {/* //todo splash screen */}
       <div
-        ref={splashScreen}
+        ref={SplashScreen}
         className="w-full h-full bg-white grid place-items-center"
       >
         <button
-          ref={splashIcon}
+          ref={SplashIcon}
           className="text-yellow-400 bg-[#10a958] rounded-full p-2 scale-0"
         >
           <LocationIcon width={80} />
         </button>
       </div>
 
-      <div ref={mainScreen} className=" hidden opacity-0 gap-2">
+      <div ref={MainScreen} className=" hidden opacity-0 gap-2">
         {/* //todo sidebar */}
         <aside className="border rounded-xl">
           <ul className="grid gap-2 auto-rows-max p-1">
