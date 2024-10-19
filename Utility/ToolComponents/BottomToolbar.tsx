5c70bde3f6a90ab95a18a0656479727e5c672c7a
Arun <arunprakashrani@gmail.com>
2024-10-19 16:43:25 +0530

    Menu bar icon as replaced insted of react icons.

--- Utility/ToolComponents/BottomToolbar.tsx
+++ Utility/ToolComponents/BottomToolbar.tsx
@@ -1,29 +1,21 @@
 "use client"
 import { change_darkmode } from "@/Store/features/pageSlice"
 import useGetMenus from "@/data/Menus"
-import { useGSAP } from "@gsap/react"
-import gsap from "gsap"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
-import React, { useRef, useState } from "react"
+import { useState } from "react"
 import { useDispatch } from "react-redux"
-
-gsap.registerPlugin(useGSAP)
 
 function BottomToolbar() {
   const { NavMenus } = useGetMenus()
   const [isDark, setDark] = useState(false)
-  const [trigger, setTrigger] = useState(false)
   const currentPath: any = usePathname()
-  const navContainer: any = useRef(null)
-  const navlist: any = useRef(null)
+
   const dispatch = useDispatch()
   const handleChange_Darkmode = () => {
     setDark(!isDark)
     dispatch(change_darkmode(!isDark))
   }
-
-  let tl = gsap.timeline({ paused: true })
 
   return (
     <div className="fixed top-2 flex justify-center gap-2 w-full z-50">
@@ -34,20 +26,22 @@ function BottomToolbar() {
         <div className="flex justify-center gap-2 items-center">
           {NavMenus?.map((links) => {
             const isActive = links?.path === currentPath
+
+            // Ensure there's a unique key for each item in the list
             return links?.name !== "Dark mode" ? (
               <Link
-                key={links?.name}
+                key={`${links?.name}-${links?.path}`} // Use a combination of name and path for unique keys
                 href={links?.path ?? ""}
                 title={links?.name}
-                className={`p-2 text-white  relative ${
-                  isActive && "  bg-lime-400 rounded-full "
+                className={`p-2 text-white relative ${
+                  isActive && "bg-lime-400 rounded-full"
                 }`}
-                type="button"
               >
                 {links?.icon}
               </Link>
             ) : (
               <button
+                key={`${links?.name}-darkmode`} // Ensure a unique key for the Dark mode button
                 onClick={handleChange_Darkmode}
                 title={links?.name}
                 className={`p-2 rounded-full text-white`}
